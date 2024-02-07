31c2cd3ca61dbc7454994fbec5bb9cdecdfa4c54
Gustavo Valmaña <gustavo.shotokai@gmail.com>
2024-02-07 12:42:08 -0500

    actualizdos tests

--- tests/Feature/CreateOrderTest.php
+++ tests/Feature/CreateOrderTest.php
@@ -20,7 +20,51 @@ class CreateOrderTest extends TestCase
 
     public function test_a_order_can_be_created()
     {
+        $data = [
 
+        ];
+        $response = $this->postJson(route('orders.store'), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'status',
+            'code'
+        ]);
+        $code_uuid = $response->json('code');
+        $this->assertDatabaseCount('orders', 1);
+        $this->assertDatabaseHas('orders', ['is_sent' => 1,'code'=> $code_uuid]);
+        $order = Order::first();
+        $this->assertEquals($order->code, $code_uuid);
+        $this->assertTrue($order->is_sent);
+    }
+
+    public function test_a_order_is_placed_on_pending_orders()
+    {
+        $data_response = [
+            'enough_stock' => false
+        ];
+        $data_request=[];
+        Http::fake([
+            'http://localhost:8001/api/get-stock-ingredients' => Http::response($data_response, 200)
+        ]);
+        $data = [
+
+        ];
+        $response = $this->postJson(route('orders.store'), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'status',
+            'code'
+        ]);
+        $code_uuid = $response->json('code');
+        $this->assertDatabaseCount('orders', 1);
+        $this->assertDatabaseHas('orders', ['is_sent' => 1,'code'=> $code_uuid, 'status' => Order::PENDING_STATUS]);
+        $order = Order::first();
+        $this->assertEquals($order->code, $code_uuid);
+        $this->assertTrue($order->is_sent);
+    }
+
+    public function test_a_order_is_placed_on_cooking_orders()
+    {
         $data_response = [
             'enough_stock' => true
         ];
@@ -39,7 +83,7 @@ class CreateOrderTest extends TestCase
         ]);
         $code_uuid = $response->json('code');
         $this->assertDatabaseCount('orders', 1);
-        $this->assertDatabaseHas('orders', ['is_sent' => 1,'code'=> $code_uuid]);
+        $this->assertDatabaseHas('orders', ['is_sent' => 1,'code'=> $code_uuid, 'status' => Order::COOKING_STATUS]);
         $order = Order::first();
         $this->assertEquals($order->code, $code_uuid);
         $this->assertTrue($order->is_sent);
