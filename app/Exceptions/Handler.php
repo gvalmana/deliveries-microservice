cbe4ee5c92994eaad677ff5a2e8dbab577193016
Gustavo Valmaña <gustavo.shotokai@gmail.com>
2024-02-07 10:05:45 -0500

    ajustadas validaciones not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +27,18 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'error' => [
+                    'message' => $exception->getMessage(),
+                    'status_code' => $exception->getStatusCode()
+                ]
+            ], $exception->getStatusCode());
+        }
+        return parent::render($request, $exception);
     }
 }
